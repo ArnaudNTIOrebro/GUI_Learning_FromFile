47aadead13fd405142d415e159bb0d41d407541c
ArnaudNTIOrebro <60583182+ArnaudNTIOrebro@users.noreply.github.com>
2022-03-07 14:45:02 +0100

    Kod med GUI  och filläsning
    Här är en kodexempel med lite GUI och inläsning av data från CSV respektive Excel.
    Namnet på comboboxar ska bytas till egna på ditt GUI om du vill att koden ska funka.

---
+++ GUI_Learning/GUI_Learning/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace GUI_Learning
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.cbxSubjectsExcel = new System.Windows.Forms.ComboBox();
+            this.lbl_Name = new System.Windows.Forms.Label();
+            this.txb_Input = new System.Windows.Forms.TextBox();
+            this.lblSubject = new System.Windows.Forms.Label();
+            this.cbxCSV = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.Location = new System.Drawing.Point(877, 42);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(203, 135);
+            this.btn_Start.TabIndex = 0;
+            this.btn_Start.Text = "Klicka på mig";
+            this.btn_Start.UseVisualStyleBackColor = true;
+           
+            // 
+            // cbxSubjectsExcel
+            // 
+            this.cbxSubjectsExcel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbxSubjectsExcel.FormattingEnabled = true;
+            this.cbxSubjectsExcel.Location = new System.Drawing.Point(22, 89);
+            this.cbxSubjectsExcel.Name = "cbxSubjectsExcel";
+            this.cbxSubjectsExcel.Size = new System.Drawing.Size(234, 37);
+            this.cbxSubjectsExcel.TabIndex = 1;
+            // 
+            // lbl_Name
+            // 
+            this.lbl_Name.AutoSize = true;
+            this.lbl_Name.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Name.Location = new System.Drawing.Point(898, 272);
+            this.lbl_Name.Name = "lbl_Name";
+            this.lbl_Name.Size = new System.Drawing.Size(169, 29);
+            this.lbl_Name.TabIndex = 2;
+            this.lbl_Name.Text = "Skriv ditt namn";
+            // 
+            // txb_Input
+            // 
+            this.txb_Input.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txb_Input.Location = new System.Drawing.Point(843, 329);
+            this.txb_Input.Name = "txb_Input";
+            this.txb_Input.Size = new System.Drawing.Size(333, 34);
+            this.txb_Input.TabIndex = 3;
+            // 
+            // lblSubject
+            // 
+            this.lblSubject.AutoSize = true;
+            this.lblSubject.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSubject.Location = new System.Drawing.Point(30, 44);
+            this.lblSubject.Name = "lblSubject";
+            this.lblSubject.Size = new System.Drawing.Size(226, 29);
+            this.lblSubject.TabIndex = 4;
+            this.lblSubject.Text = "Välj ett ämne(Excel)";
+            // 
+            // cbxCSV
+            // 
+            this.cbxCSV.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbxCSV.FormattingEnabled = true;
+            this.cbxCSV.Location = new System.Drawing.Point(22, 198);
+            this.cbxCSV.Name = "cbxCSV";
+            this.cbxCSV.Size = new System.Drawing.Size(231, 37);
+            this.cbxCSV.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(32, 147);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(177, 29);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Välj från CSV fil";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1690, 845);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbxCSV);
+            this.Controls.Add(this.lblSubject);
+            this.Controls.Add(this.txb_Input);
+            this.Controls.Add(this.lbl_Name);
+            this.Controls.Add(this.cbxSubjectsExcel);
+            this.Controls.Add(this.btn_Start);
+            this.Name = "Form1";
+            this.Text = "GUI inlärning";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Start;
+        private System.Windows.Forms.ComboBox cbxSubjectsExcel;
+        private System.Windows.Forms.Label lbl_Name;
+        private System.Windows.Forms.TextBox txb_Input;
+        private System.Windows.Forms.Label lblSubject;
+        private System.Windows.Forms.ComboBox cbxCSV;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ GUI_Learning/GUI_Learning/Form1.cs
@@ -0,0 +1,135 @@
+﻿using DocumentFormat.OpenXml.Spreadsheet;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+using IronXL;
+
+namespace GUI_Learning
+{
+    public partial class Form1 : Form
+    {
+        Excel.Application ExcelObj = null;
+        Excel.Workbooks wbooks = null;
+        Excel.Workbook workbook = null;
+        Excel.Worksheet worksheet = null;       
+        Excel.Sheets sheets = null;             
+        public static string strFilePath = "";
+        public static string strFileName = "";
+        public Form1()
+        {
+            InitializeComponent();
+            ExcelObj = new Excel.Application();
+            strFilePath = "Subjects1.xlsx";           
+            wbooks = ExcelObj.Workbooks;
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<string> dataFromCSV =  GetDataFromCSVFile();
+            Populate_CbxData_WithList(dataFromCSV, cbxCSV); // namnet på "cbxCSV" är annorlunda för dig.
+
+            List<string> subjectsFromExcel = GetExcelData_Directly();
+            Populate_CbxData_WithList(subjectsFromExcel, cbxSubjectsExcel);// namnet på "cbxSubjectsExcel" är annorlunda för dig.             
+        }
+
+        private List<string> GetDataFromCSVFile()
+        {
+            List<string>  subjectsFromCSV = new List<string>();
+
+            try   //här kan du byta namnet "subjects" i parameter med namnet "subjects2" för att generera fel i undantag.
+            {                
+                var CSVData = File.ReadAllLines("subjects.csv");
+
+                foreach (var data in CSVData)
+                {
+                    var strSubject = data.Split(',');
+                    subjectsFromCSV.Add(strSubject[2]);
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Problem with CSV : " + e.ToString());
+            }
+            
+            return subjectsFromCSV;
+        }
+
+        private List<string> GetExcelData_Directly()
+        {
+            List<string> subjectsFromExcel = new List<string>();
+            string cellContent = String.Empty;
+            int currentSheet = 1;
+            string strFilePath2 = String.Empty;
+
+            try       //här kan du byta namnet "strFilePath" i parameter med namnet "strFilePath2" för att generera fel i undantag.
+            {
+                workbook = wbooks.Open(strFilePath, 0, false, 5, "", "", true, Excel.XlPlatform.xlWindows, "\t", true, false, 0, true, true, true);
+                sheets = workbook.Worksheets;
+
+                worksheet = (Excel.Worksheet)sheets.get_Item(currentSheet);
+
+                for (int i = 2; i < 20; i++)
+                {
+                    cellContent = worksheet.Cells[i, 1].Value;
+
+                    if (!String.IsNullOrEmpty(cellContent))
+                    {
+                        subjectsFromExcel.Add(cellContent);
+                    }
+                }
+            }
+            catch(Exception e) 
+            {
+                MessageBox.Show("Problem med Excel dok: " + e.ToString());
+            }
+            finally
+            {
+                releaseObject(worksheet);
+                releaseObject(sheets);
+                releaseObject(workbook);
+                releaseObject(wbooks);
+                releaseObject(ExcelObj);                
+            }
+            return subjectsFromExcel;
+        }
+
+        private void Populate_CbxData_WithList(List<string> subjects, ComboBox combobox )
+        {
+            List<string> Local_Subjects = subjects;
+
+            for (int i = 0; i < Local_Subjects.Count; i++)
+            {
+                combobox.Items.Add(Local_Subjects[i]);
+            }
+        }
+        private void releaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+                MessageBox.Show("Unable to release the Object " + ex.ToString());
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }       
+    }
+
+}
+    
+
